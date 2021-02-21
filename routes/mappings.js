0163440f0e38301ad2946323dfca6bcd2f3bda75
Jerry Shockley <noise1@shockleynet.com>
2021-02-21 12:30:34 -0500

    Added post and get /:id routes
    Still debugging.

--- routes/mappings.js
+++ routes/mappings.js
@@ -1,10 +1,39 @@
 const express = require(`express`)
 const router = express.Router()
+// const sql = require(`/bin/index.js`)
+const data = []
+let respondentId = 0
 
 router.get(`/new`, (req, res) => {
-  res.render(`mapping`, { title: `Relationship Mapping` })
+  res.render(`mapping`, {
+    jsFile: `mapping_new.js`,
+    title: `New Relationship Mapping`,
+  })
 })
 
+router.post(`/`, (req, res) => {
+  // req.body.forEach((mapping) => {})
+  respondentId += 1
+  data.push(req.body)
+  res.status(200)
+  // res.json({
+  //   message: `Data saved successfully.`,
+  //   location: `/mappings/${respondentId}`,
+  // })
+  res.render(`mapping/${respondentId}`)
+})
+
+router.get(`/:id`, (res, req) => {
+  // respondent = Respondent.build
+  const { id } = req.params
+  const strPoints = JSON.stringify(data[id - 1])
+  console.log(strPoints)
+  res.render(`/mapping`, {
+    points: strPoints,
+    jsFile: `mapping_show.js`,
+    title: `Relationship Mapping for: ${respondentId}`,
+  })
+})
 /* GET users listing. */
 // router.get(`/`, (req, res) => {
 //   res.send(`respond with a resource`)
