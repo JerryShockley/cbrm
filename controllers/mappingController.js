0b7e96deceee1573699c79e43c738872096a1512
Jerry Shockley <noise1@shockleynet.com>
2021-02-24 15:41:24 -0500

    Renamed controllers and fixed bugs.
    Show path is still not coming up.

--- controllers/mappingController.js
+++ controllers/mappingController.js
@@ -4,25 +4,25 @@ const data = []
 let respondentId = 0
 
 exports.mappingNew = (req, res) => {
-  res.render(`mapping/edit`, {
+  res.render(`mapping/new`, {
     title: `New Relationship Mapping`,
     myprompt: `A very good prompt that tells you what to do!`,
   })
 }
 
-exports.mappingPost = (req, res) => {
-  console.log(JSON.Stringify(`req.body`, null, 2))
+exports.mappingCreate = (req, res) => {
+  console.log(JSON.stringify(req.body, null, 2))
   respondentId += 1
-  data.push(req.body)
-  res.render(`mapping/show/${respondentId}`)
+  data.push(req.body.points)
+  res.redirect(`${respondentId}`)
 }
 
 exports.mappingShow = (req, res) => {
   // respondent = Respondent.build
-  console.log(strPoints)
+  console.log(`Received: ID = ${req.params.id}`)
   const { id } = req.params
   res.render(`mapping/show`, {
     points: data[id - 1],
-    title: `Relationship Mapping for: ${respondentId}`,
+    title: `Relationship Mapping for: ${id}`,
   })
 }
