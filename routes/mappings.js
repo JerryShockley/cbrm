b54daf919126d4675eae90be57c598c118509d23
Jerry Shockley <noise1@shockleynet.com>
2021-02-24 13:21:01 -0500

    Added controllers for the routes.

--- routes/mappings.js
+++ routes/mappings.js
@@ -1,42 +1,12 @@
 const express = require(`express`)
 const router = express.Router()
-// const sql = require(`/bin/index.js`)
-const data = []
-let respondentId = 0
+const controller = require(`../controllers/mappingController`)
 
-router.get(`/new`, (req, res) => {
-  res.render(`mapping`, {
-    jsFile: `mapping_new.js`,
-    title: `New Relationship Mapping`,
-  })
-})
-
-router.post(`/`, (req, res) => {
-  // req.body.forEach((mapping) => {})
-  respondentId += 1
-  data.push(req.body)
-  res.status(200)
-  // res.json({
-  //   message: `Data saved successfully.`,
-  //   location: `/mappings/${respondentId}`,
-  // })
-  res.render(`mapping/${respondentId}`)
-})
-
-router.get(`/:id`, (res, req) => {
-  // respondent = Respondent.build
-  const { id } = req.params
-  const strPoints = JSON.stringify(data[id - 1])
-  console.log(strPoints)
-  res.render(`/mapping`, {
-    points: strPoints,
-    jsFile: `mapping_show.js`,
-    title: `Relationship Mapping for: ${respondentId}`,
-  })
-})
-/* GET users listing. */
-// router.get(`/`, (req, res) => {
-//   res.send(`respond with a resource`)
-// })
+// Create new mapping
+router.get(`/new`, controller.mappingNew)
+// Post date from new mapping
+router.post(`/`, controller.mappingPost)
+// Show mapping
+router.get(`/:id`, controller.mappingShow)
 
 module.exports = router
