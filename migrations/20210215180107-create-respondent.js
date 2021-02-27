79e4774a33af13bda8fa462917e19934110a0ada
Jerry Shockley <noise1@shockleynet.com>
2021-02-27 08:20:12 -0500

    Added pointCount and duration columns.

--- migrations/20210215180107-create-respondent.js
+++ migrations/20210215180107-create-respondent.js
@@ -1,32 +1,50 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable(`respondents`, {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      respondentId: {
-        type: Sequelize.STRING,
-      },
-      projectId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: `projects`,
-          key: `id`,
+    try {
+      await queryInterface.createTable(`respondents`, {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
         },
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    })
+        respondentId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+        },
+        pointCount: {
+          type: Sequelize.INTEGER,
+          defaultValue: 0,
+        },
+        duration: {
+          type: Sequelize.INTEGER,
+          defaultValue: 0,
+        },
+        projectId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: `projects`,
+            key: `id`,
+          },
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      })
+      const seqQuery = `ALTER SEQUENCE "respondents_respondentId_seq" restart with 1000`
+      await queryInterface.sequelize.query(seqQuery, `RAW`)
+
+      return Promise.resolve()
+    } catch (err) {
+      return Promise.reject(err)
+    }
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable(`respondents`)
