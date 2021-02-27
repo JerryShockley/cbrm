8da52f63ff2736acb8e3ba6aa8dda21ad95a7c65
Jerry Shockley <noise1@shockleynet.com>
2021-02-27 08:14:24 -0500

    Revert "Added pointCount and duration columns."
    This reverts commit c6feb36de623a4a5a4cb3dc2ef082f5a75e9c1e5.

--- migrations/20210215170309-create-project.js
+++ migrations/20210215170309-create-project.js
@@ -1,38 +1,28 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    try {
-      await queryInterface.createTable(`projects`, {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-        },
-        name: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        projectId: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          autoIncrement: true,
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-      })
-      const seqQuery = `ALTER SEQUENCE "projects_projectId_seq" restart with 100`
-      await queryInterface.sequelize.query(seqQuery, `RAW`)
-
-      return Promise.resolve()
-    } catch (err) {
-      return Promise.reject(err)
-    }
+    await queryInterface.createTable(`projects`, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      projectId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      brand: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable(`projects`)
