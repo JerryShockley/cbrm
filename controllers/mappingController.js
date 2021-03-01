817e3f3aa64cda736d99fde85eea240c77aeff7b
Jerry Shockley <noise1@shockleynet.com>
2021-03-01 12:48:34 -0500

    Corrected Promise usage, Added duration to local show view data.

--- controllers/mappingController.js
+++ controllers/mappingController.js
@@ -1,7 +1,6 @@
-const Model = require(`../models/mapping`)
-// const sql = require(`/bin/index.js`)
-const data = []
-let respondentId = 0
+const db = require(`../models/index`)
+const Mapping = require(`../models/mapping`)
+const { nextRespondentId } = require(`../lib/idFactory`)
 
 exports.mappingNew = (req, res) => {
   res.render(`mapping/new`, {
@@ -10,18 +9,43 @@ exports.mappingNew = (req, res) => {
   })
 }
 
-exports.mappingCreate = (req, res) => {
+exports.mappingCreate = async (req, res) => {
   console.log(JSON.stringify(req.body, null, 2))
-  respondentId += 1
-  data.push(req.body.points)
-  res.json({ location: `/mappings/${respondentId}` })
+  const { points } = req.body
+  db.Respondent.create(
+    {
+      projectId: 1,
+      respondentId: nextRespondentId(),
+      duration: db.Respondent.sumMappingDurations(points),
+      pointCount: db.Respondent.getMappingsCount(points),
+      mappings: points,
+    },
+    {
+      include: [`mappings`],
+    }
+  )
+    .then((respondent) => {
+      console.log(`finished create`)
+      res.json({ location: `/mappings/${respondent.id}` })
+    })
+    .catch((err) => {
+      console.error(`failed to create respondent: ${err.message}`)
+      process.kill(process.pid, `sigint`)
+    })
 }
 
-exports.mappingShow = (req, res) => {
+exports.mappingShow = async (req, res) => {
   // respondent = Respondent.build
   const { id } = req.params
-  res.render(`mapping/show`, {
-    points: data[id - 1],
-    title: `Relationship Mapping for Respondent #${id}`,
-  })
+  db.Respondent.findByPk(id, { include: `mappings` })
+    .then((respondent) => {
+      res.render(`mapping/show`, {
+        points: respondent.mappings,
+        duration: respondent.getDurationString(),
+        title: `Relationship Mapping for Respondent #${respondent.respondentId}`,
+      })
+    })
+    .catch((err) => {
+      console.error(`Failed to find Respondent: ${error}`)
+    })
 }
