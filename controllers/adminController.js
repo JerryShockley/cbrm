d2934ac0a5cd446c9e2ab7022cfd56b35d65e689
Jerry Shockley <noise1@shockleynet.com>
2021-03-17 16:51:01 -0400

    Initial Admin dashboard

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,14 @@
 const db = require(`../models/index`)
 
+exports.adminDashboard = (req, res) => {
+  res.render(`admin/dashboard`, {
+    title: `Administrator Dashboard`
+  })
+}
+
 exports.adminRespondentNewSearch = (req, res) => {
     res.render(`admin/new_respondent_search`, {
-      title: `Respondent Search`
+      title: `Respondent Search`,
   })
 }
 
@@ -11,20 +17,41 @@ exports.adminRespondentCreateSearch = (req, res) => {
   db.Respondent.findOne({
     where: {
       respondentId: parseInt(id, 10)
-    },
-    include: [ `mappings` ]
+    }
   })
     .then((respondent) => {
-      res.render(`respondent/show`, {
-        points: respondent.mappings,
-        duration: respondent.getDurationString(),
-        createDate: respondent.createdAt,
-        totalPoints: respondent.pointCount,
-        title: `Respondent #${respondent.respondentId}`,
+      res.redirect(`/respondents/${respondent.id}`)
+      })
+    .catch((err) => {
+      res.render(`admin/new_respondent_search`, {
+        title: `Respondent Search`,
+        errmsg: `Error: Respondent ${id} was not found`,
       })
     })
-    .catch((err) => {
-      console.error(`Failed to find Respondent: ${err}`)
-    })
+}
 
+exports.adminProjectNewSearch = (req, res) => {
+    res.render(`admin/new_project_search`, {
+      title: `Project Search`,
+  })
+}
+
+exports.adminProjectCreateSearch = (req, res) => {
+  const { projectId: id } = req.body
+  db.Project.findOne({
+    where: {
+      projectId: parseInt(id, 10)
+    }
+  })
+    .then((project) => {
+      console.log(JSON.stringify(project, null, 2))
+      res.redirect(`/projects/${project.id}`)
+      })
+    .catch((err) => {
+      console.error(`Failed to find Project: ${err}`)
+      res.render(`admin/new_project_search`, {
+        title: `Project Search`,
+        errmsg: `Error: Project ${id} was not found`,
+      })
+    })
 }
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,10 @@ const express = require(`express`)
 const router = express.Router()
 const controller = require(`../controllers/adminController`)
 
+router.get(`/`, controller.adminDashboard)
 router.get(`/search/respondent/new`, controller.adminRespondentNewSearch)
 router.post(`/search/respondent/create`, controller.adminRespondentCreateSearch)
+router.get(`/search/project/new`, controller.adminProjectNewSearch)
+router.post(`/search/project/create`, controller.adminProjectCreateSearch)
 
 module.exports = router
