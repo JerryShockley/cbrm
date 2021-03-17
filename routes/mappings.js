b4a6d6d80808c5f5439063f84ada5e6bb07fbb11
Jerry Shockley <noise1@shockleynet.com>
2021-03-17 17:20:29 -0400

    Moved all route to Respondent.

--- routes/mappings.js
+++ routes/mappings.js
@@ -1,12 +1,4 @@
 const express = require(`express`)
 const router = express.Router()
-const controller = require(`../controllers/mappingController`)
-
-// Create new mapping
-router.get(`/new`, controller.mappingNew)
-// Post date from new mapping
-router.post(`/create`, controller.mappingCreate)
-// Show mapping
-router.get(`/:id`, controller.mappingShow)
 
 module.exports = router
