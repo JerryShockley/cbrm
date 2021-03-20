7e994eee65da2fa1080930453e258de1003f7b68
Jerry Shockley <noise1@shockleynet.com>
2021-03-20 11:31:29 -0400

    Added 3355 suffix to admin path: /admin/3355 for improved security.

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require(`express`)
 const router = express.Router()
 const controller = require(`../controllers/adminController`)
 
-router.get(`/`, controller.adminDashboard)
+router.get(`/3355`, controller.adminDashboard)
 router.get(`/search/respondent/new`, controller.adminRespondentNewSearch)
 router.post(`/search/respondent/create`, controller.adminRespondentCreateSearch)
 router.get(`/search/project/new`, controller.adminProjectNewSearch)
