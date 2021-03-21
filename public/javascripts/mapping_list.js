29a4f968227770717540bd82ec42ae2fd7c28175
Jerry Shockley <noise1@shockleynet.com>
2021-03-21 12:56:02 -0400

    Added respondentId, buttons and projected right-aligned numbers.

--- public/javascripts/mapping_list.js
+++ public/javascripts/mapping_list.js
@@ -1,14 +1,15 @@
 $(`#mapping-table`).DataTable({
   ajax: `/mappings/listdata`,
   dom: `Bfrtip`,
-  buttons: [`copy`, `csv`, `excel`, `pdf`, `print`],
+  buttons: [`copy`, `csv`, `excel`, `pdf`, `print`, `pageLength`],
   columns: [
     { data: `brand` },
-    { data: `order` },
-    { data: `cartesianX` },
-    { data: `cartesianY` },
-    { data: `duration` },
-    { data: `respondent_id` },
+    { data: `order`, className: `txtright` },
+    { data: `cartesianX`, className: `txtright` },
+    { data: `cartesianY`, className: `txtright` },
+    { data: `duration`, className: `txtright` },
+    { data: `respondent.respondentId`, className: `txtright` },
+    { data: `respondent.project.projectId`, className: `txtright` },
     { data: `createdAt` },
   ],
 })
