5607bc28ca76332472f1dbada3db51b7ba0b8771
Jerry Shockley <noise1@shockleynet.com>
2021-03-21 12:52:17 -0400

    Added include and nested include to get respondentId and projectId.

--- controllers/mappingController.js
+++ controllers/mappingController.js
@@ -9,8 +9,22 @@ exports.mappingList = (req, res) => {
 exports.mappingListData = (req, res) => {
   db.Mapping.findAll({
     attributes: {
-      exclude: [`id`, `notes`, `updatedAt`],
+      exclude: [`id`, `notes`, `updatedAt`, `respondent_id`],
     },
+    include: [
+      {
+        model: db.Respondent,
+        as: `respondent`,
+        attributes: [`respondentId`],
+        include: [
+          {
+            model: db.Project,
+            as: `project`,
+            attributes: [`projectId`],
+          },
+        ],
+      },
+    ],
   })
     .then((mappings) => {
       console.log(JSON.stringify(mappings[0], null, 2))
