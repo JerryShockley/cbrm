50b80cd3afb6897491b402e97a70210894958725
Jerry Shockley <noise1@shockleynet.com>
2021-03-21 12:59:05 -0400

    Added  start to the root / path to deal with express route select prob.

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ const controller = require(`../controllers/indexController`)
 /* GET home page. */
 router.get(`/`, controller.homePage)
 router.get(`/finish/:id`, controller.nextSteps)
-router.get(`/:id`, controller.startSurvey)
+router.get(`/start/:id`, controller.startSurvey)
 
 module.exports = router
