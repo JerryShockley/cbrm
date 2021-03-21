60ce020e8b5f0098c1921e6abfba6335f869db91
Jerry Shockley <noise1@shockleynet.com>
2021-03-21 12:57:04 -0400

    buttons and projected right-aligned numbers.

--- public/javascripts/respondent_list.js
+++ public/javascripts/respondent_list.js
@@ -1,13 +1,14 @@
 $(`#respondent-table`).DataTable({
   ajax: `/respondents/listdata`,
+  responsive: true,
   dom: `Bfrtip`,
-  buttons: [`copy`, `csv`, `excel`, `pdf`, `print`],
+  buttons: [`copy`, `csv`, `excel`, `pdf`, `print`, `pageLength`],
   columns: [
-    { data: `respondentId` },
-    { data: `pointCount` },
-    { data: `durationSum` },
-    { data: `project_id` },
+    { data: `respondentId`, className: `txtright text-wrap` },
+    { data: `pointCount`, className: `txtright` },
+    { data: `durationSum`, className: `txtright` },
+    { data: `project.projectId`, className: `txtright` },
+    { data: `MappingsCount`, className: `txtright` },
     { data: `createdAt` },
-    { data: `MappingsCount` },
   ],
 })
