8179e1ca1d874f79ec203bcd2fb20950318604c1
Jerry Shockley <noise1@shockleynet.com>
2021-04-05 09:04:53 -0400

    Removed admin and respondent search after adding functionality to lists.

--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,5 @@ const router = express.Router()
 const controller = require(`../controllers/adminController`)
 
 router.get(`/3355`, controller.adminDashboard)
-router.get(`/search/respondent/new`, controller.adminRespondentNewSearch)
-router.post(`/search/respondent/create`, controller.adminRespondentCreateSearch)
-router.get(`/search/project/new`, controller.adminProjectNewSearch)
-router.post(`/search/project/create`, controller.adminProjectCreateSearch)
 
 module.exports = router
