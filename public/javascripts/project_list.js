c3633d01083cbae15d030abece4f30232416fc01
Jerry Shockley <noise1@shockleynet.com>
2021-04-05 09:09:59 -0400

    Added Id anchor link.

--- public/javascripts/project_list.js
+++ public/javascripts/project_list.js
@@ -1,9 +1,24 @@
+/* eslint-disable */
+const cport = window.location.port
+const cportStr = cport ? `:${cport}` : ``
+const showUrlRoot = `${window.location.protocol}//${window.location.hostname}${cportStr}`
+
 $(`#project-table`).DataTable({
   ajax: `/projects/listdata`,
   dom: `Bfrtip`,
   buttons: [`copy`, `csv`, `excel`, `pdf`, `print`, `pageLength`],
   columns: [
-    { data: `projectId`, className: `txtright` },
+    {
+      data: `projectId`,
+      className: `txtright`,
+      render: function (data, type, row, meta) {
+        // prettier-ignore
+        const link = "<a class='list-link' href='" + showUrlRoot + "/projects/" +
+          row.id + "'>" + data + "</a>"
+        // alert(JSON.stringify(row))
+        return link
+      },
+    },
     { data: `name` },
     { data: `createdAt` },
     { data: `updatedAt` },
--- public/javascripts/respondent_list.js
+++ public/javascripts/respondent_list.js
@@ -1,10 +1,25 @@
+/* eslint-disable */
+const cport = window.location.port
+const cportStr = cport ? `:${cport}` : ``
+const showUrlRoot = `${window.location.protocol}//${window.location.hostname}${cportStr}`
+
 $(`#respondent-table`).DataTable({
   ajax: `/respondents/listdata`,
   responsive: true,
   dom: `Bfrtip`,
   buttons: [`copy`, `csv`, `excel`, `pdf`, `print`, `pageLength`],
   columns: [
-    { data: `respondentId`, className: `txtright text-wrap` },
+    {
+      data: `respondentId`,
+      className: `txtright text-wrap`,
+      render: function (data, type, row, meta) {
+        // prettier-ignore
+        const link = "<a class='list-link' href='" + showUrlRoot + "/respondents/" +
+          row.id + "'>" + data + "</a>"
+        // alert(JSON.stringify(row))
+        return link
+      },
+    },
     { data: `pointCount`, className: `txtright` },
     { data: `durationSum`, className: `txtright` },
     { data: `project.projectId`, className: `txtright` },
